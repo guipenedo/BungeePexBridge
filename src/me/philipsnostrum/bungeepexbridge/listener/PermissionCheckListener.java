34aa70b84da2f37423674052084244e50873b7f9
PhilipsNostrum <nostrumg@gmail.com>
2016-07-03 20:18:43 +0100

    Add check for revoked permissions
    Now when a permission is granted bungeepexbridge will check if it has
    been revoked (declared with a - on that group's permissions)

--- src/me/philipsnostrum/bungeepexbridge/listener/PermissionCheckListener.java
+++ src/me/philipsnostrum/bungeepexbridge/listener/PermissionCheckListener.java
@@ -1,6 +1,7 @@
 package me.philipsnostrum.bungeepexbridge.listener;
 
 import me.philipsnostrum.bungeepexbridge.BungeePexBridge;
+import me.philipsnostrum.bungeepexbridge.modules.PermGroup;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PermissionCheckEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -13,6 +14,11 @@ public class PermissionCheckListener implements Listener {
         if (BungeePexBridge.getDB().enabled && e.getSender() instanceof ProxiedPlayer) {
             if (!e.hasPermission()) {
                 e.setHasPermission(BungeePexBridge.get().hasPermission(((ProxiedPlayer) e.getSender()).getUniqueId(), e.getPermission()));
+            }
+
+            PermGroup permGroup = PermGroup.getPlayerGroup(((ProxiedPlayer) e.getSender()).getUniqueId());
+            if (e.hasPermission() && permGroup != null){
+                e.setHasPermission(!permGroup.getRevoked().contains(e.getPermission()));
             }
         }
     }
